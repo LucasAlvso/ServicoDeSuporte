c399bef6d2da3da575bd8cad51791fdb4f9993d5
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-04-15 21:52:52 -0300

    Implemented all default methods of ChamadoManager

--- src/main/java/ChamadoManager.java
+++ src/main/java/ChamadoManager.java
@@ -1,32 +1,122 @@
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class ChamadoManager {
 
-	private List<Chamado> chamados;
+	private final List<Chamado> chamados;
 
-	public List<Chamado> findChamadoByFuncionario(Funcionario funcionario) {
-		return null;
+
+	public ChamadoManager() {
+		this.chamados = new LinkedList<>();
 	}
 
-	public List<Chamado> findChamadosByEquipamento(Equipamento equipamento) {
-		return null;
+	public List<Chamado> findChamadoByFuncionario(Funcionario funcionario)
+	{
+		List<Chamado> chamadosByFuncionario = new LinkedList<>();
+
+		for (Chamado chamado : chamados)
+		{
+			if (chamado.getFuncionario().equals(funcionario))
+			{
+				chamadosByFuncionario.add(chamado);
+			}
+		}
+
+		return chamadosByFuncionario;
 	}
 
-	public boolean mudarStatusDeChamado(Chamado chamado) {
+	public List<Chamado> findChamadosByEquipamento(Equipamento equipamento)
+	{
+		List<Chamado> chamadosByEquipamento = new LinkedList<>();
+
+		for (Chamado chamado : chamados)
+		{
+			if (chamado.getEquipamento().equals(equipamento))
+			{
+				chamadosByEquipamento.add(chamado);
+			}
+		}
+
+		return chamadosByEquipamento;
+	}
+
+	public boolean mudarStatusDeChamado(Chamado chamado)
+	{
+		for (Chamado existingChamado : chamados)
+		{
+			if (existingChamado.equals(chamado))
+			{
+				if (existingChamado.getStatus().equalsIgnoreCase("ABERTO"))
+				{
+					existingChamado.setStatus("EM ANDAMENTO");
+					return true;
+				}
+				else if (existingChamado.getStatus().equalsIgnoreCase("EM ANDAMENTO"))
+				{
+					existingChamado.setStatus("CONCLUIDO");
+					return true;
+				}
+				else
+				{
+					System.out.println("Chamado já está concluído");
+					return false;
+				}
+			}
+		}
+
 		return false;
 	}
 
-	public List<Chamado> findChamadosByKeyword(String keyword) {
-		return null;
+	public List<Chamado> findChamadosByKeyword(String keyword)
+	{
+		List<Chamado> chamadosByKeyword = new LinkedList<>();
+
+		for (Chamado chamado : chamados)
+		{
+			if (chamado.getEquipamento().getDescricao().contains(keyword) ||
+					chamado.getFuncionario().getNome().contains(keyword) ||
+					chamado.getFuncionario().getDepartamento().contains(keyword) ||
+					chamado.getTextoResolucao().contains(keyword) ||
+					chamado.getTextoSolitacao().contains(keyword))
+			{
+				chamadosByKeyword.add(chamado);
+			}
+		}
+
+		return chamadosByKeyword;
 	}
 
-	public double getPorcentagemChamadosByStatus() {
-		return 0;
+	public double getPorcentagemChamadosByStatus(String status)
+	{
+		int count = 0;
+
+		for (Chamado chamado : chamados)
+		{
+			if (chamado.getStatus().equalsIgnoreCase(status))
+			{
+				count++;
+			}
+		}
+
+		return ((double) count / chamados.size()) * 100;
 	}
 
-	public double getMediaChamadosPorDia(Date data) {
-		return 0;
+	public double getMediaChamadosPorDia(Date data)
+	{
+		int countChamados = 0;
+
+		Set<Date> distinctDias = new HashSet<>();
+
+		for (Chamado chamado : chamados)
+		{
+			if (chamado.getDataAbertura().equals(data))
+			{
+				countChamados++;
+			}
+
+			distinctDias.add(chamado.getDataAbertura());
+		}
+
+		return ((double) countChamados / distinctDias.size());
 	}
 
 }
