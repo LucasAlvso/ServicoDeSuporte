46a4a806a2883d361002e6853b43abf3759ea5bf
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-04-15 21:27:26 -0300

    Created constructor, getters and setters for Chamado

--- src/main/java/Chamado.java
+++ src/main/java/Chamado.java
@@ -12,4 +12,62 @@ public class Chamado {
 
 	private String status;
 
+	public Chamado(Funcionario funcionario, Equipamento equipamento, int dataSolicitacao, Date dataAbertura, String status)
+	{
+		this.funcionario = funcionario;
+		this.equipamento = equipamento;
+		this.dataSolicitacao = dataSolicitacao;
+		this.dataAbertura = dataAbertura;
+		this.status = status;
+	}
+
+	public Funcionario getFuncionario()
+	{
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario)
+	{
+		this.funcionario = funcionario;
+	}
+
+	public Equipamento getEquipamento()
+	{
+		return equipamento;
+	}
+
+	public void setEquipamento(Equipamento equipamento)
+	{
+		this.equipamento = equipamento;
+	}
+
+	public int getDataSolicitacao()
+	{
+		return dataSolicitacao;
+	}
+
+	public void setDataSolicitacao(int dataSolicitacao)
+	{
+		this.dataSolicitacao = dataSolicitacao;
+	}
+
+	public Date getDataAbertura()
+	{
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(Date dataAbertura)
+	{
+		this.dataAbertura = dataAbertura;
+	}
+
+	public String getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
 }
