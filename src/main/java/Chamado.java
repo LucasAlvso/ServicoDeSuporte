f94d11b035e34672ec8b12b91d71196c69c54797
LucasAlvso <102501862+LucasAlvso@users.noreply.github.com>
2023-04-23 13:09:12 -0300

    Chamado manager implementation (#1)
    * Created constructor, getters and setters for Chamado
    
    * Deleted String class mistakenly generated by Astah
    
    * Added getters, setters, constructors and necessary attributes
    
    * Implemented all default methods of ChamadoManager
    
    * Implemented add method in ChamadoManager

--- src/main/java/Chamado.java
+++ src/main/java/Chamado.java
@@ -1,3 +1,4 @@
+import java.util.Collection;
 import java.util.Date;
 
 public class Chamado {
@@ -12,4 +13,77 @@ public class Chamado {
 
 	private String status;
 
+	private String textoResolucao;
+
+	private String textoSolicitacao;
+
+	public Chamado(Funcionario funcionario, Equipamento equipamento, int dataSolicitacao, Date dataAbertura, String textoSolicitacao)
+	{
+		this.funcionario = funcionario;
+		this.equipamento = equipamento;
+		this.dataSolicitacao = dataSolicitacao;
+		this.dataAbertura = dataAbertura;
+		this.status = "ABERTO";
+		this.textoSolicitacao = textoSolicitacao;
+	}
+
+	public Funcionario getFuncionario()
+	{
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario)
+	{
+		this.funcionario = funcionario;
+	}
+
+	public Equipamento getEquipamento()
+	{
+		return equipamento;
+	}
+
+	public void setEquipamento(Equipamento equipamento)
+	{
+		this.equipamento = equipamento;
+	}
+
+	public int getDataSolicitacao()
+	{
+		return dataSolicitacao;
+	}
+
+	public void setDataSolicitacao(int dataSolicitacao)
+	{
+		this.dataSolicitacao = dataSolicitacao;
+	}
+
+	public Date getDataAbertura()
+	{
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(Date dataAbertura)
+	{
+		this.dataAbertura = dataAbertura;
+	}
+
+	public String getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+
+	public String getTextoResolucao()
+	{
+		return textoResolucao;
+	}
+
+	public String getTextoSolitacao()
+	{
+		return textoSolicitacao;
+	}
 }
