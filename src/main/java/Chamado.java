13426d2fb885f7b732542b150c329131772894d8
LucasAlvso <102501862+LucasAlvso@users.noreply.github.com>
2023-05-01 14:33:45 -0300

    Develop (#9)
    * Chamado manager implementation (#1)
    
    * Created constructor, getters and setters for Chamado
    
    * Deleted String class mistakenly generated by Astah
    
    * Added getters, setters, constructors and necessary attributes
    
    * Implemented all default methods of ChamadoManager
    
    * Implemented add method in ChamadoManager
    
    * fix: changed String status to Enum type (#2)
    
    * Implementei corpo do método
    
    * Implementei corpo do método
    
    * hotfix: fixed setter for resolution text & added `FuncionarioDeSuporte` related to each `Chamado` (#3)
    
    * feature: creating support employee class
    
    * feat: implemented funcionario manager methods
    
    * fix: added missing get to resolution text
    
    * Esqueleto da CLI e método para listar chamados por equipamento.
    
    * Mudei retorno do método listarChamadosPorEquipamento.
    
    * Sobrescrevi o construtor padrão da classe SystemManager para evitar nullpointer com as classes manager. (#8)
    
    ---------
    
    Co-authored-by: Gustavo P. Scholze <scholzegustavo@gmail.com>
    Co-authored-by: Gabriel Bohn <gabrielcbohn@gmail.com>
    Co-authored-by: Gabriel Cecconi Bohn <59932874+GabrielBohn99@users.noreply.github.com>

--- src/main/java/Chamado.java
+++ src/main/java/Chamado.java
@@ -1,6 +1,10 @@
+import java.util.Collection;
 import java.util.Date;
 
 public class Chamado {
+	public enum Status{
+		ABERTO, ANDAMENTO, CONCLUIDO;
+	}
 
 	private Funcionario funcionario;
 
@@ -10,6 +14,85 @@ public class Chamado {
 
 	private Date dataAbertura;
 
-	private String status;
+	private Status status;
 
+	private String textoResolucao;
+
+	private String textoSolicitacao;
+
+	private FuncionarioDeSuporte FuncionarioSuporte;
+
+	public Chamado(Funcionario funcionario, Equipamento equipamento, int dataSolicitacao, Date dataAbertura, String textoSolicitacao)
+	{
+		this.funcionario = funcionario;
+		this.equipamento = equipamento;
+		this.dataSolicitacao = dataSolicitacao;
+		this.dataAbertura = dataAbertura;
+		this.status = Status.ABERTO;
+		this.textoSolicitacao = textoSolicitacao;
+	}
+
+	public Funcionario getFuncionario()
+	{
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario)
+	{
+		this.funcionario = funcionario;
+	}
+
+	public Equipamento getEquipamento()
+	{
+		return equipamento;
+	}
+
+	public void setEquipamento(Equipamento equipamento)
+	{
+		this.equipamento = equipamento;
+	}
+
+	public int getDataSolicitacao()
+	{
+		return dataSolicitacao;
+	}
+
+	public void setDataSolicitacao(int dataSolicitacao)
+	{
+		this.dataSolicitacao = dataSolicitacao;
+	}
+
+	public Date getDataAbertura()
+	{
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(Date dataAbertura)
+	{
+		this.dataAbertura = dataAbertura;
+	}
+
+	public Status getStatus()
+	{
+		return this.status;
+	}
+
+	public void setStatus(Status status)
+	{
+		this.status = status;
+	}
+
+	public void setTextoResolucao(String textoResolucao){
+		this.textoResolucao = textoResolucao;
+	}
+
+	public String getTextoSolitacao()
+	{
+		return textoSolicitacao;
+	}
+
+  public String getTextoResolucao()
+	{
+		return textoResolucao;
+	}
 }
--- src/main/java/FuncionarioManager.java
+++ src/main/java/FuncionarioManager.java
@@ -1,15 +1,36 @@
-import java.util.List;
+import java.util.*;
 
 public class FuncionarioManager {
+	private final List<Funcionario> funcionarios;
 
-	private List<Funcionario> funcionarios;
+  public FuncionarioManager() {
+      funcionarios = new LinkedList<>();
+  }
+
+  public boolean addFuncionario(Funcionario employee) {
+    Funcionario idAlreadyInUse = findFuncionarioById(employee.getId());
+
+    if (idAlreadyInUse != null) {
+      return false;
+    }
+
+    return funcionarios.add(employee);
+  }
+
+  public List<Funcionario> getFuncionarios() {
+    return funcionarios;
+  }
 
 	public Funcionario findFuncionarioById(int id) {
-		return null;
-	}
+    for (Funcionario funcionario : funcionarios)
+		{
+			if (funcionario.getId() == id)
+			{
+				return funcionario;
+			}
+		}
 
-	public List<Funcionario> findFuncionariosBySetor(String setor) {
-		return null;
+    return null;
 	}
 
 }
