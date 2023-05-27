3ab229cab63119001b05e2573e1e48162a70921e
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-05-27 13:25:15 -0300

    Actions failing

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,4 @@
 public class Application
 {
-    public static void main(String[] args)
-    {
-        SystemManager systemManager = new SystemManager();
-        systemManager.inserirDadosIniciais();
-        systemManager.runCliMenu();
-
-    }
 
 }
